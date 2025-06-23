64c330f007fcdd4a85c4a994d4d65581f1ec50c5
Shivam A. Tadas <124132894+shivam-tadas@users.noreply.github.com>
2025-06-23 22:38:30 +0530

    Time: 27 ms (34.48%) | Memory: 17.8 MB (55.32%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        self.candidates = candidates
+        self.target = target
+        self.combinations = []
+
+        self.cs2Helper([], -1, 0)
+        return self.combinations
+
+    def cs2Helper(self, path: List[int], start: int, pathSum: int) -> None:
+        if pathSum >= self.target:
+            if (pathSum == self.target) and (path not in self.combinations):
+                self.combinations.append(path)
+
+            return
+
+        i = start + 1
+        while i < len(self.candidates):
+            if i > start + 1 and self.candidates[i] == self.candidates[i - 1]:
+                i += 1
+                continue
+
+            self.cs2Helper(path + [self.candidates[i]], i, pathSum + self.candidates[i])
+            i += 1
