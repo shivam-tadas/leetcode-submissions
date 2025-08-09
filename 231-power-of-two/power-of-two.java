afb12bad366af68012bb628656d6c2f29aa69283
Shivam A. Tadas <124132894+shivam-tadas@users.noreply.github.com>
2025-08-09 11:00:45 +0530

    Time: 1 ms (35.75%) | Memory: 40.7 MB (91.35%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,9 +1,8 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         if (n == 0) return false;
-        while (n%2 == 0){
+        while (n%2 == 0)
             n >>= 1;
-        }
         return n == 1;
     }
 }
