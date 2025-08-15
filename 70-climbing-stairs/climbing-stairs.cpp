8ca87338590b16376858d68bc2045d9af7b338d2
Shivam A. Tadas <124132894+shivam-tadas@users.noreply.github.com>
2025-08-15 20:07:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (36.88%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < n+1; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        // Print the dp array
+        // cout << "[ ";
+        // for (int i: dp) cout << i << ' ';
+        // cout << ']' << endl;
+        return dp[n];
+    }
+};
